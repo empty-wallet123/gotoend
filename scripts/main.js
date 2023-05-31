b234da685c96e3a522013c4bb9f1625647e8edbe
empty-wallet123 <127477455+empty-wallet123@users.noreply.github.com>
2023-05-31 15:17:14 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+/*const myHeading = document.querySelector('h1');*/
+/*
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
+*/
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  }
+multiply(4,3);
+/*
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me! 别点我！");
+  }); 
+*/
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  let changed = true
+  if (mySrc === "images/IMG_0521.jpg" & changed)  {
+    myImage.setAttribute("src", "images/cat2.jpg");
+    changed = false;
+  } 
+  if (mySrc === "images/cat2.jpg" & changed) {
+    myImage.setAttribute("src", "images/cat3.jfif");
+    changed = false;
+  }  
+  if (mySrc === "images/cat3.jfif" & changed) {
+    myImage.setAttribute("src", "images/IMG_0521.jpg");
+    changed = false;
+  }  
+  }
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if(!myName){
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `网页设计很酷吧, ${myName}`;
+  }
+  }
+  
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `网页设计很酷吧, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
